5110496c20f9c7fa96a58fcbf0b74e32533a089a
aconitum001 <skandern0000@gmail.com>
2024-05-30 18:31:29 +0100

    add title and subtitle to details

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
@@ -23,6 +24,7 @@ abstract class Styles {
 
   static const textStyle30 = TextStyle(
     fontSize: 30,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
+    fontFamily: kGtSectraFine,
   );
 }
--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:bookly/core/utils/constants.dart';
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
@@ -15,9 +17,31 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+            padding: EdgeInsets.symmetric(horizontal: width * 0.18),
             child: const CustomBookImage(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            "The Jungle Book",
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          Text(
+            "Rudyard Kipking",
+            style: Styles.textStyle18.copyWith(
+              fontWeight: FontWeight.w500,
+              color: Colors.white.withOpacity(0.7),
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          const BookRating(),
         ],
       ),
     );
