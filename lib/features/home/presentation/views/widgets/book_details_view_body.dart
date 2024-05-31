f6a0a9bf4807e6d67c70fb625cec9c78fc064198
aconitum001 <skandern0000@gmail.com>
2024-05-31 16:09:18 +0100

    refactor book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,7 @@
 import 'package:bookly/core/utils/constants.dart';
-import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/features/home/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
-import 'package:bookly/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -12,64 +9,24 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
-          hasScrollBody: false,
           child: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: kPadding, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: kPadding, vertical: 20),
             child: Column(
               children: [
-                const CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * 0.18),
-                  child: const CustomBookImage(),
-                ),
-                const SizedBox(
-                  height: 38,
-                ),
-                const Text(
-                  "The Jungle Book",
-                  style: Styles.textStyle30,
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  "Rudyard Kipking",
-                  style: Styles.textStyle18.copyWith(
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white.withOpacity(0.7),
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const BookRating(),
-                const SizedBox(
+                CustomBookDetailsAppBar(),
+                BookDetailsSection(),
+                SizedBox(
                   height: 30,
                 ),
-                const BooksAction(),
-                const Expanded(
+                Expanded(
                   child: SizedBox(
                     height: 50,
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "You can also like",
-                    style: Styles.textStyle16
-                        .copyWith(fontWeight: FontWeight.w600),
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const SimilarBooksListView()
+                SimilarBooksSection(),
               ],
             ),
           ),
---
+++ lib/features/home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,48 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.18),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(
+          height: 38,
+        ),
+        const Text(
+          "The Jungle Book",
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(
+          "Rudyard Kipking",
+          style: Styles.textStyle18.copyWith(
+            fontWeight: FontWeight.w500,
+            color: Colors.white.withOpacity(0.7),
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        const BookRating(),
+        const SizedBox(
+          height: 37,
+        ),
+        const BooksAction(),
+      ],
+    );
+  }
+}
